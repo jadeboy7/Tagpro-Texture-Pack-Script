89087ee4dff5d826d2e1b36cbbb944d8e3ec36f0
Shimon Berger <jadeboy7@gmail.com>
2015-01-08 00:51:09 -0500

    Initial Commit

---
+++ TagPro_Texture_Pack.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name          TagPro Texture Pack
+// @description   Texture Pack for TagPro
+// @version       1
+// @grant         none
+// @include       http://tagpro-*.koalabeast.com:*
+// @include       http://tangent.jukejuice.com:*
+// @include       http://*.newcompte.fr:*
+// @license       2015
+// @author        jadeboy7
+// @namespace     http://www.reddit.com/user/jadeboy7
+// ==/UserScript==
+
+tagpro.ready(function() {
+    $('#tiles')[0].src = 'http://i.imgur.com/LgQzVYF.png';
+    $('#splats')[0].src = 'http://i.imgur.com/PJEMNnN.png';
+    $('#speedpad')[0].src = 'http://i.imgur.com/V8D62iX.png';
+    $('#speedpadred')[0].src = 'http://i.imgur.com/2Y8FREi.png';
+    $('#speedpadblue')[0].src = 'http://i.imgur.com/61yNlAe.png';
+    $('#portal')[0].src = 'http://i.imgur.com/AAknDsI.png';
+});
